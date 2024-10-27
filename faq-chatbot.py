635f1b9fdeaea1f92483e008c3c2af467c04aa62
KorRyu3 <KorRyu3>
2024-10-27 08:02:34 +0000

    LangChain経由で推論する

--- faq-chatbot.py
+++ faq-chatbot.py
@@ -7,7 +7,11 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install mlflow==2.10.1 lxml==4.9.3 transformers==4.30.2 langchain==0.1.5 databricks-vectorsearch==0.22 databricks-sdk==0.28.0 databricks-feature-store==0.17.0 openai
+# MAGIC %pip install langchain==0.2.11 langchain-core==0.2.23 langchain-community==0.2.10
+
+# COMMAND ----------
+
+# MAGIC %pip install mlflow==2.10.1 lxml==4.9.3 transformers==4.30.2 databricks-vectorsearch==0.22 databricks-sdk==0.28.0 databricks-feature-store==0.17.0
 # MAGIC %pip install dspy-ai -U
 
 # COMMAND ----------
@@ -324,152 +328,108 @@ import mlflow
 import mlflow.deployments
 
 from databricks.vector_search.client import VectorSearchClient
-from langchain_core.prompts.chat import HumanMessagePromptTemplate
-from openai import OpenAI
+# from langchain_core.prompts.chat import HumanMessagePromptTemplate
+from langchain_core.prompts import (
+    PromptTemplate,
+    ChatPromptTemplate,
+)
+from langchain_core.runnables import RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import DatabricksVectorSearch
+from langchain_core.chat_models import ChatDatabricks
+# from openai import OpenAI
 
-# RAGチェーンのお作法
-# PyTorchみたいにやる
-class AirbricksRAGAgentApp(mlflow.pyfunc.PythonModel):
+mlflow.langchain.autolog()
 
-    def __init__(self):
-        """
-        コンストラクタ
-        """
+############
+# Helper functions
+############
+# Return the string contents of the most recent message from the user
+def extract_user_query_string(chat_messages_array):
+    return chat_messages_array[-1]["content"]
 
-        self.model_config = mlflow.models.ModelConfig(development_config="rag_chain_config.yaml")
 
-        try:
-            # サービングエンドポイントのホストに"DB_MODEL_SERVING_HOST_URL"が自動設定されるので、その内容をDATABRICKS_HOSTにも設定
-            os.environ["DATABRICKS_HOST"] = os.environ["DB_MODEL_SERVING_HOST_URL"]
-        except:
-            pass
+# Return the chat history, which is is everything before the last question
+def extract_chat_history(chat_messages_array):
+    return chat_messages_array[:-1]
 
-        vsc = VectorSearchClient(disable_notice=True)
-        self.vs_index = vsc.get_index(
-            endpoint_name=self.model_config.get("vector_search_endpoint_name"),
-            index_name=self.model_config.get("vector_search_index_name")
+
+model_config = mlflow.models.ModelConfig(development_config="rag_chain_config.yaml")
+
+# VectorSearchIndexと接続
+vs_client = VectorSearchClient(disable_notice=True)
+vs_index = vs_client.get_index(
+    endpoint_name=rag_chain_config.get("vector_search_endpoint_name"),
+    index_name=rag_chain_config.get("vector_search_index_name")
+)
+
+
+vector_search_as_retriever = DatabricksVectorSearch(
+    index=vs_index,
+    text_column="response",
+    columns=[
+        "id",
+        "query",
+        "response",
+        "url",
+    ],
+).as_retriever(search_kwargs={"k": 5, 'query_type': 'ann'})
+
+
+mlflow.models.set_retriever_schema(
+    primary_key="id",
+    text_column="response",
+    doc_uri="url",
+)
+
+# chunk毎のコンテキストを整形する
+def format_context(docs):
+    chunk_template ="Passage: {chunk_text}\n"
+    chunk_contents = [
+        chunk_template.format(
+            chunk_text=d.page_content,
+            document_uri=d.metadata["url"],
         )
+        for d in docs
+    ]
+    return "".join(chunk_contents)
 
-        # 特徴量サービングアクセス用クライアントの取得
-        self.deploy_client = mlflow.deployments.get_deploy_client("databricks")
-
-        # LLM基盤モデルのエンドポイントのクライアントを取得
-        self.chat_model = OpenAI(
-            api_key=os.environ.get("DATABRICKS_TOKEN"),
-            base_url=os.environ.get("DATABRICKS_HOST") + "/serving-endpoints",
-        )
-
-        # システムプロンプトを準備
-        self.SYSTEM_MESSAGE = "【参考情報】のみを参考にしながら【質問】にできるだけ正確に答えてください。わからない場合や、質問が適切でない場合は、分からない旨を答えてください。【参考情報】に記載されていない事実を答えるのはやめてください。"
-
-        # ヒューマンプロンプトテンプレートを準備
-        human_template = """【参考情報】
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "【参考情報】のみを参考にしながら【質問】にできるだけ正確に答えてください。わからない場合や、質問が適切でない場合は、分からない旨を答えてください。【参考情報】に記載されていない事実を答えるのはやめてください。"),
+        ("user", """【参考情報】
 {context}
 
 【質問】
-{question}"""
-        self.HUMAN_MESSAGE = HumanMessagePromptTemplate.from_template(human_template)
+{question}"""),
+    ]
+)
 
-        
-    def _find_relevant_doc(self, question, num_results = 10, relevant_threshold = 0.0):
-        """
-        ベクター検索インデックスにリクエストを送信し、類似コンテンツを検索
+model = ChatDatabricks(
+    endpoint=model_config.get("llm_endpoint_name"),
+    extra_params={"temperature": 0.01, "max_token": 1500},
+)
 
-        relevant_threshold: コサイン類似度の閾値
-        """
+from operator import itemgetter
+chain = (
+    {
+        "question": itemgetter("messages") | RunnableLambda(extract_user_query_string),
+        "context": itemgetter("messages")
+        | RunnableLambda(extract_user_query_string)
+        | vector_search_as_retriever,
+        | RunnableLambda(format_context),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
 
-        results = self.vs_index.similarity_search(
-            query_text=question,
-            columns=["query", "response"],
-            num_results=num_results)
-
-        docs = results.get('result', {}).get('data_array', [])
-
-        #関連性スコアでフィルタリングします。0.7以下は、関連性の高いコンテンツがないことを意味する
-        returned_docs = []
-        for doc in docs:
-          if doc[-1] > relevant_threshold:
-            returned_docs.append({"query": doc[0], "response": doc[1]})
-
-        return returned_docs
-
-
-    def _build_prompt(self, docs, question):
-        """
-        プロンプトの構築
-        """
-
-        context = ""
-        for doc in docs:
-          context = context + doc['response'] + "\n\n"
-
-        human_message = self.HUMAN_MESSAGE.format_messages(
-          context=context, 
-          question=question
-        )
-    
-        prompt=[
-            {
-                "role": "system",
-                "content": self.SYSTEM_MESSAGE
-            },
-            {
-                "role": "user",
-                "content": human_message[0].content,
-            }
-        ]
-
-        return prompt
-
-
-    @mlflow.trace(name="predict_rag")
-    def predict(self, context, model_input, params=None):
-        """
-        推論メイン関数
-        """
-
-        if isinstance(model_input, pd.DataFrame):
-            model_input = model_input.to_dict(orient="records")[0]
-
-        # FAQデータからベクター検索を用いて質問と類似している情報を検索
-        # with句の中の実行時間や入力, 出力データをログに残せる
-        with mlflow.start_span(name="_find_relevant_doc") as span:  # logをとる
-            question = model_input["messages"][-1]["content"]
-            docs = self._find_relevant_doc(question)
-            span.set_inputs({"question": question})
-            span.set_outputs({"docs": docs})
-
-        # プロンプトの構築
-        with mlflow.start_span(name="_build_prompt") as span:  # logをとる
-            prompt = self._build_prompt(docs, question)
-            span.set_inputs({"question": question, "docs": docs})
-            span.set_outputs({"prompt": prompt})
-
-        # LLMに回答を生成させる
-        with mlflow.start_span(name="generate_answer") as span:  # logをとる
-            response = self.chat_model.chat.completions.create(
-                model=self.model_config.get("llm_endpoint_name"),
-                messages=prompt,
-                max_tokens=2000,
-                temperature=0.1
-            )
-            span.set_inputs({"question": question, "prompt": prompt})
-            span.set_outputs({"answer": response})
-        
-        
-        # 回答データを整形して返す.
-        # ChatCompletionResponseの形式で返さないと後々エラーとなる。
-        return response.to_dict()
-
-# Chroma: ベクトルインデックスをローカルに作って、毎度呼び出しても効率いいかも？
-
-mlflow.models.set_model(model=AirbricksRAGAgentApp())
+mlflow.models.set_model(model=chain)
 
 # COMMAND ----------
 
 input_example = {
-  "messages": [{"role": "user", "content": "授業時間はどのくらいですか？"}]
+  "messages": [{"role": "user", "content": "授業時間は一コマどのくらいですか？"}]
 }
 
-rag_model = AirbricksRAGAgentApp()
-rag_model.predict(None, model_input=input_example)
+chain.invoke(input_example)
