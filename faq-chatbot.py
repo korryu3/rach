39a1f2700125df3e29df8607a02a6db50e7ba646
146335193+KorRyu3@users.noreply.github.com <146335193+KorRyu3@users.noreply.github.com>
2025-01-07 13:25:25 +0000

    refac: contextual retrievalの重複処理を削除

--- faq-chatbot.py
+++ faq-chatbot.py
@@ -416,31 +416,27 @@ import time
 from tqdm import tqdm
 processes_list = []
 failed_list = []
+
 for doc in tqdm(raw_data_dict):
-    processed_raw_data_dict = {}
     try:
         # たまにmlflowでkeyerror:'content'という謎のエラーが出るので、ここでtry-catchしておく
         processed_content = process_and_annotate_document(doc['content'], doc['page_contents'])
-        processed_raw_data_dict = {
-            'content': processed_content,
-            'page_contents': doc['page_contents'],
-            'url': doc['url'],
-        }
-        processes_list.append(processed_raw_data_dict)
-        time.sleep(0.5)
     except Exception as e:
-        print(e)
         failed_list.append(doc)
         time.sleep(5)
-
         processed_content = process_and_annotate_document(doc['content'], doc['page_contents'])
-        processed_raw_data_dict = {
-            'content': processed_content,
-            'page_contents': doc['page_contents'],
-            'url': doc['url'],
-        }
-        processes_list.append(processed_raw_data_dict)
-        time.sleep(0.5)
+
+    processed_raw_data_dict = {
+        'content': processed_content,
+        'page_contents': doc['page_contents'],
+        'url': doc['url'],
+    }
+    processes_list.append(processed_raw_data_dict)
+    time.sleep(0.5)
+
+# COMMAND ----------
+
+failed_list
 
 # COMMAND ----------
 
